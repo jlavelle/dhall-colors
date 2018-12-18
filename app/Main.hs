8448b0d6b0e3ae8f8442d90772664ac3c92fbcac
John C. Lavelle <jlavelle604@gmail.com>
2018-12-17 20:13:00 -0500

    Make it total

--- app/Main.hs
+++ app/Main.hs
@@ -11,8 +11,10 @@ import Text.HTML.TagSoup.Tree (TagTree(..))
 import qualified Text.HTML.TagSoup as Tag
 import Text.HTML.TagSoup ((~==), (~/=), Tag(..))
 import Text.HTML.TagSoup.Tree (TagTree)
-import Control.Arrow ((&&&))
-import Data.Maybe (fromJust)
+import Control.Arrow ((&&&), (<<<))
+import Control.Monad ((<=<))
+import Data.Bitraversable (bisequenceA)
+import Data.Maybe (fromJust, catMaybes)
 
 parsePage :: IO [(ByteString, ByteString)]
 parsePage = do
@@ -20,7 +22,8 @@ parsePage = do
   r  <- HTTP.parseRequest "https://en.wikipedia.org/wiki/List_of_colors_(compact)"
   getColors . Tag.parseTags . HTTP.responseBody <$> HTTP.httpLbs r m
  where
-  getColors = fmap ((getColor . head) &&& (getName . last))
+  getColors = catMaybes
+            . fmap (bisequenceA <<< (getColor <=< headM) &&& (getName <=< lastM))
             . fmap (\t -> [x | x@(TagBranch "p" _ _) <- Tree.universeTree t])
             . (\t -> [ cs | (TagBranch "div" _ cs) <- Tree.universeTree t ])
             . Tree.tagTree
@@ -28,10 +31,19 @@ parsePage = do
             . tail
             . dropWhile (~/= ("<h3>" :: String))
    where
-    getColor (TagBranch _ as _) = fromJust $ lookup "title" as
-    getName (TagBranch _ _ t) = f . head $ concat [ cs | x@(TagBranch "a" _ cs) <- Tree.universeTree t ]
+    getColor (TagBranch _ as _) = lookup "title" as
+    getName (TagBranch _ _ t) = fmap f . headM $ hasUrl <> noUrl
      where
+      hasUrl = concat [ cs | (TagBranch "a" _ cs) <- Tree.universeTree t ]
+      noUrl  = [ x | x@(TagLeaf _) <- Tree.universeTree t]
       f (TagLeaf (TagText x)) = x
+
+headM :: [a] -> Maybe a
+headM []    = Nothing
+headM (a:_) = Just a
+
+lastM :: [a] -> Maybe a
+lastM = headM . reverse
 
 main :: IO ()
 main = print "implement me pls"
